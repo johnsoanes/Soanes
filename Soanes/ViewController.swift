d467c27573b685341cc07cf637258ff80a40de43
John Soanes <johnsoanes@MacBook-Pro.local>
2017-01-01 12:47:34 +0000

    Initial Commit

---
+++ Soanes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Soanes
+//
+//  Created by John Soanes on 01/01/2017.
+//  Copyright © 2017 John Soanes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
